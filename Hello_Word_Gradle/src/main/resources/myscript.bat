df7114bd137e29ffa2e1bed9749baa79286311e4
Prashant Kulkarni <prashantklk7@gmail.com>
2016-05-11 20:55:23 +0530

    srcipt edit

--- Hello_Word_Gradle/src/main/resources/myscript.bat
+++ Hello_Word_Gradle/src/main/resources/myscript.bat
@@ -1,7 +1,8 @@
+@ECHO OFF
 echo "Executing my shell script"
 
 set JAVA_HOME=C:\Program Files\Java\jre7
-set LIB_DIR=D:\gradle_projects\Hello_Word_Gradle\build\libs
+set LIB_DIR=D:\gradle_proj\git_repo\Gradle_Script\Hello_Word_Gradle\build\libs
 set CLASSPATH=C:\Program Files\Java\jdk1.7.0_79\bin
 set CLASSPATH=%CLASSPATH%;%LIB_DIR%\Hello_Word_Gradle.jar
 
